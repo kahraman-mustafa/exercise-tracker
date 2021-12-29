2b37ffcec4eaa69f4d4ca8d4aa971e5211799393
Mustafa Kahraman <kahraman@Mustafas-MacBook-Pro.local>
2021-12-29 21:55:56 +0300

    routes are added. Ready to first API test via Postman

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -2,17 +2,30 @@ const router = require('express').Router();
 let Exercise = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
-  User.find()
-    .then(users => res.json(users))
+  // mongoose find() method returns all list of all model data, here i.e. all exercises
+  Exercise.find()
+    .then(exercises => res.json(exercises))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
   const username = req.body.username;
+  const description = req.body.description;
+  const duration = Number(req.body.duration);
+  const date = Date.parse(req.body.date);
 
-  const newUser = new User({username});
+  // creating a new instance of a mongoose data model
+  const newExercise = new Exercise({
+    username,
+    description,
+    duration,
+    date
+  });
 
-  newUser.save()
-    .then(() => res.json('User added!'))
+  // mongoose save() command, saves newly created model instance to Mongo database
+  newExercise.save()
+    .then(() => res.json('Exercise added!'))
     .catch(err => res.status(400).json('Error: ' + err));
-}) 
+});
+
+module.exports = router; 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
+  // mongoose find() method returns all list of all model data, here i.e. all users
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
@@ -10,9 +11,13 @@ router.route('/').get((req, res) => {
 router.route('/add').post((req, res) => {
   const username = req.body.username;
 
+  // creating a new instance of a mongoose data model
   const newUser = new User({username});
 
+  // mongoose save() command, saves newly created model instance to Mongo database
   newUser.save()
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err));
-}) 
+}) ;
+
+module.exports = router; 

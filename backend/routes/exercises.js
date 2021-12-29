80540c063cf4dca1ac9b52587fbc11484bcc6f7f
Mustafa Kahraman <kahraman@Mustafas-MacBook-Pro.local>
2021-12-29 22:32:50 +0300

    Backend code working, Postman tested.

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -28,4 +28,35 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+  // mongoose findById() method returns corresponding exercise according to the id provided by user as a url param
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  // mongoose findByIdAndDelete() method deletes corresponding exercise according to the id provided by user as a url param
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(exercise => res.json("Exercise deleted."))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  // mongoose findByIdAndDelete() method returns corresponding exercise according to the id provided by user as a url param
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      // mongoose save() command, saves newly created model instance to Mongo database
+      exercise.save()
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router; 
